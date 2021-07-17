bee4f2a9db14096bbff5022e98f7b0b92b1c4b4c
Hein Pieterse <hp.pieterse@gmail.com>
2021-07-17 03:37:36 +0200

    Add sass modules and dashboard component

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 npm install in client app
 dotnet dev-certs https --trust
+
+dotnet watch run -p ./avodash/avodash.csproj 
---
+++ avodash/ClientApp/src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,10 @@
+import React  from 'react';
+import Styles from './Dashboard.module.scss';
+
+var Dashboard = () => {
+  return (
+    <div className={Styles.Heading}>Hello World</div>
+  )
+}
+
+export default Dashboard;
